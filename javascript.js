ce45e17cd7c19d5fc8729c6a6315ebf2af1e5c1a
Luis Puga <duhluis.p@gmail.com>
2022-03-31 16:14:51 -0400

    If applied, this commit will include boiler plate code for the rock paper scissors project from the odin project.

---
+++ javascript.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+function computerPlay(){
+    let random = ["rock", "paper", "scrissors"];
+    return random[Math.floor(Math.random() * 3)];
+}
+
+console.log(random);
