2b0e8889f9dd80a45e74126da01227963688fbc7
Luis Puga <duhluis.p@gmail.com>
2022-04-18 13:51:14 -0400

    if applied this commit will improve the logic of the game by shortening the logic that is associated with winning and losing. This is working

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Title for the console
 console.log("Hello World, Welcome to Luis Puga Rock Paper Scissors");
 
 /* This function takes care of generating the random output */
@@ -7,42 +8,38 @@ function computerPlay(){
 }
 
 /* Ask the user for input */
-var userChoice = prompt("Do you choose rock, paper or scissors?");
 // This line will convert the input to lowercase so that the program is NOT case sensative
-userChoice = userChoice.toLowerCase();
+const playerSelection =  prompt("Do you choose rock, paper or scissors?").toLowerCase();
+
+// This line gets the computer selection
+const computerSelection = computerPlay();
 
 /* Compare the input to the randomly generated computer input */
 /* Return a result of win or loss for to the console */
-computerChoice = computerPlay()
-//This line is for testing purposes to see what the function generated so to check for a bug
-console.log(computerChoice)
-//This line will compare the results for a tie
-if(userChoice == computerChoice){
-    "Tie"
-}
-//These lines will check for other scenerios
-if(userChoice == "rock"){
-    if(computerChoice == "paper"){
-        console.log("You lose")
+function playRound(playerSelection, computerSelection) {
+    //Play five games
+    //This line will compare the results for a tie
+    let result = "";
+    //This line will validate the entry for a win scenerio
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        //This line displays the win method
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection)
     }
-    else{
-        console.log("You Win")
+    //This line checks for a tie scenerio
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection)
     }
-}
-if(userChoice == "paper"){
-    if(computerChoice == "scrissors"){
-        console.log("You lose")
-    }
-    else{
-        console.log("You Win")
-    }
-}
-if(userChoice == "scrissors"){
-    if(computerChoice == "rock"){
-        console.log("You lose")
-    }
-    else{
-        console.log("You Win")
-    }
-}
+    //This final else statement is for the scenerio when the user loses
+    else {
+        
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection)
 
+       
+    }
+    console.log(result)
+}//End of play round
+
+//This line will print the results
+console.log(playRound(playerSelection, computerSelection));
